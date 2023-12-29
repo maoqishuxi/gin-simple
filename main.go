12392cbc866c85e19139b92070985adc31b740a5
maoshu <maoqishuxi@gmail.com>
2023-12-29 15:03:21 +0800

    gin embed finished

--- main.go
+++ main.go
@@ -1,7 +1,48 @@
 package main
 
-import "fmt"
+import (
+	"embed"
+	"io/fs"
+	"log"
+	"net/http"
+
+	"github.com/gin-contrib/static"
+	"github.com/gin-gonic/gin"
+)
+
+//go:embed build/*
+var app embed.FS
+
+func EmbedReact(urlPrefix, buildDirectory string, em embed.FS) gin.HandlerFunc {
+	dir := static.LocalFile(buildDirectory, true)
+	embedDir, _ := fs.Sub(em, buildDirectory)
+	fileserver := http.FileServer(http.FS(embedDir))
+
+	if urlPrefix != "" {
+		fileserver = http.StripPrefix(urlPrefix, fileserver)
+	}
+
+	return func(c *gin.Context) {
+		if !dir.Exists(urlPrefix, c.Request.URL.Path) {
+			c.Request.URL.Path = "/"
+		}
+		fileserver.ServeHTTP(c.Writer, c.Request)
+		c.Abort()
+	}
+}
 
 func main() {
-	fmt.Println("welcome to you")
+
+	gin.SetMode(gin.ReleaseMode)
+
+	router := gin.New()
+	router.Use(gin.Recovery())
+	router.Use(gin.Logger())
+
+	router.Use(EmbedReact("/", "build", app))
+
+	port := ":8000"
+	log.Printf("Listening and serving HTTP on %s\n", port)
+
+	router.Run(port)
 }
